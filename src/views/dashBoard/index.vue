fbf118be55ac04ae6ce132289e908715c4ba0f6a
liwp <liwp@hsmap.com>
2020-11-16 18:03:28 +0800

    feat: add useFullScreen hooks

--- src/views/dashBoard/index.vue
+++ src/views/dashBoard/index.vue
@@ -1,13 +1,26 @@
 <template>
-  <div class="container">仪表盘</div>
+  <div class="container">
+    仪表盘
+    <div class="box">
+      <a-space>
+        <a-tag color="purple">
+          {{ isFullscreen ? '当前为全屏' : '当前非全屏' }}
+        </a-tag>
+        <a-switch v-model:checked="isFullscreen" />
+        <a-button size="small" type="primary" @click="toggleFull">toggle</a-button>
+      </a-space>
+    </div>
+  </div>
 </template>
 
 <script>
 import { ref, reactive, watch, computed, toRefs } from 'vue'
+import useFullscreen from '/@/hooks/useFullscreen'
 export default {
   name: 'DashBoard',
   setup() {
-    return {}
+    const { isFullscreen, setFull, exitFull, toggleFull } = useFullscreen()
+    return { isFullscreen, setFull, exitFull, toggleFull }
   }
 }
 </script>
